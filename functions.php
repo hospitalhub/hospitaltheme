9759c2e64f197224f5ab4d003a8269009c7fe459
amarcinkowski <andrzej.marcinkowski@gmail.com>
2015-11-05 15:42:39 +0100

    font awesome

--- functions.php
+++ functions.php
@@ -6,4 +6,13 @@ $sas->init(HOSPITAL_PLUGIN_URL, array(''), array('bootstrap.min.js'), array('boo
 
 include 'src/media_perms.php';
 
+
+// 
+
+function hospital_scripts() {
+//	wp_enqueue_style( 'fontawesome', '//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css'__FILE__ );
+}
+
+add_action( 'wp_enqueue_scripts', 'hospital_scripts' );
+
 ?>
