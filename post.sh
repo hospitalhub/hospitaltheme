4073072a6eec6bdb0801e6e47a14e180960265d7
amarcinkowski <andrzej.marcinkowski@gmail.com>
2015-11-10 15:14:42 +0100

    files

--- post.sh
+++ post.sh
@@ -1,1 +1,28 @@
-wp post create $1
+CATEGORIES=(team page slider)
+for CATEGORY in ${CATEGORIES[@]}; do
+ for f in $CATEGORY/*;do
+	echo "adding $f"
+	n="${f/$CATEGORY\//}"
+	x=`wp post create "$f" --post_title="$n" --post_status=publish --porcelain`; 
+	echo "id $x"
+	i="${f/$CATEGORY/images\/$CATEGORY}"
+	if [ -e "$i".jpg ]; then
+		echo "pic: $i.jpg"
+		type="jpg"
+	elif [ -e "$i".png ]; then
+		echo "pic: $i.png"
+		type="png"
+	else
+		type=
+	fi
+	# if image exists
+	if [ -z "$type" ]; then
+		echo "no pic for $i"
+	else
+ 		wp media import "$i"."$type" --post_id=`echo $x` --featured_image
+	fi
+	# add category
+	wp post term add `echo $x` category $CATEGORY 
+	wp post term add `echo $x` post_tag $CATEGORY
+ done
+done
