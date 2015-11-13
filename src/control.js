370ca9d4d8f63796317b02ada43db84c514edfe1
amarcinkowski <andrzej.marcinkowski@gmail.com>
2015-11-13 17:00:28 +0100

    nowe pliki

---
+++ src/control.js
@@ -0,0 +1,12 @@
+jQuery(document).ready(function($) {
+	// stop slider on info
+        $("#infodis").click( function() {
+//          var slider = $('#main-slider .bx-slider').bxSlider();
+//	  slider.goToSlide(1);			          
+	});
+	// start slider on ok
+  /*      $(".ok").click( function() {
+	        var slider = $('#main-slider .bx-slider').bxSlider();
+		slider.startAuto();			          
+	});*/
+});
