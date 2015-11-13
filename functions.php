5a765df337a9c3b62ad03c7b730d911753f64bc9
amarcinkowski <andrzej.marcinkowski@gmail.com>
2015-11-13 16:58:27 +0100

    jquery

--- functions.php
+++ functions.php
@@ -6,11 +6,11 @@ $sas->init(HOSPITAL_PLUGIN_URL, array(''), array('bootstrap.min.js'), array('boo
 
 include 'src/media_perms.php';
 
-
 // 
 
 function hospital_scripts() {
 //	wp_enqueue_style( 'fontawesome', '//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css'__FILE__ );
+	wp_enqueue_script( 'control', get_stylesheet_directory_uri() . '/src/control.js', array('jquery'));
 }
 
 add_action( 'wp_enqueue_scripts', 'hospital_scripts' );
