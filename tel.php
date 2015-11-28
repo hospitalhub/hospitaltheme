8f75868adb93b425288dfbdbd92e163b7447c411
amarcinkowski <andrzej.marcinkowski@gmail.com>
2015-11-28 22:22:00 +0100

    tel iframe

--- tel.php
+++ tel.php
@@ -10,7 +10,9 @@ get_header();
  *
  * @package accesspress_parallax
  */
-include('/book/index.php');
+?>
+<iframe align="center" width="100%" height="400px" src="http://127.0.0.1:8000/vendor/addressbook/addressbook" frameborder="yes" scrolling="yes" name="myIframe" id="myIframe"> </iframe>
 
+<?php
 get_footer();
 ?>
