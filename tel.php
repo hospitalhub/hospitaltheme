53b5aaa99b82f0ff54a4d59eb2bbde2578d27d94
amarcinkowski <andrzej.marcinkowski@gmail.com>
2015-11-28 20:09:59 +0100

    tel

---
+++ tel.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Template Name: TEL
+ *
+ * @package accesspress_parallax_child
+ */
+get_header();
+/**
+ * The template for displaying all Parallax Templates.
+ *
+ * @package accesspress_parallax
+ */
+include('/book/index.php');
+
+get_footer();
+?>
