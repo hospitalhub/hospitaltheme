faffd5cd8729e7a8087740182773499f36f06b68
amarcinkowski <andrzej.marcinkowski@gmail.com>
2015-11-29 17:52:52 +0100

    phoneb

--- src/orgchart.js
+++ src/orgchart.js
@@ -6,48 +6,6 @@ jQuery(document)
 		.ready(
 				function($) {
 
-
-
-
-
-
-
-
-$(function () {
-    $('#table').bootstrapTable({
-        data: data
-    });
-
-    $(".mybtn-top").click(function () {
-        $('#table').bootstrapTable('scrollTo', 0);
-    });
-    
-    $(".mybtn-row").click(function () {
-        var index = +$('.row-index').val(),
-            top = 0;
-        $('#table').find('tbody tr').each(function (i) {
-                if (i < index) {
-                top += $(this).height();
-            }
-        });
-        $('#table').bootstrapTable('scrollTo', top);
-    });
-    
-    $(".mybtn-btm").click(function () {
-        $('#table').bootstrapTable('scrollTo', 'bottom');
-    });
-
-});
-
-
-
-
-
-
-
-
-
-
 					$("#people")
 							.getOrgChart(
 									{
