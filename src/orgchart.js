d1f1807ca411d239e45efa3f68996507440aa81a
amarcinkowski <andrzej.marcinkowski@gmail.com>
2015-11-29 17:47:11 +0100

    change name

--- src/orgchart.js
+++ src/orgchart.js
@@ -1,6 +1,52 @@
+
+
+
+
 jQuery(document)
 		.ready(
 				function($) {
+
+
+
+
+
+
+
+
+$(function () {
+    $('#table').bootstrapTable({
+        data: data
+    });
+
+    $(".mybtn-top").click(function () {
+        $('#table').bootstrapTable('scrollTo', 0);
+    });
+    
+    $(".mybtn-row").click(function () {
+        var index = +$('.row-index').val(),
+            top = 0;
+        $('#table').find('tbody tr').each(function (i) {
+                if (i < index) {
+                top += $(this).height();
+            }
+        });
+        $('#table').bootstrapTable('scrollTo', top);
+    });
+    
+    $(".mybtn-btm").click(function () {
+        $('#table').bootstrapTable('scrollTo', 'bottom');
+    });
+
+});
+
+
+
+
+
+
+
+
+
 
 					$("#people")
 							.getOrgChart(
